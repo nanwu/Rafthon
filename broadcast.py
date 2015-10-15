571486b81d180c9e28575f396346d595d2616cbe
nanwu <nanbytesflow@gmail.com>
2015-10-15 12:35:34 -0500

    heartbeat broadcast

--- broadcast.py
+++ broadcast.py
@@ -2,24 +2,36 @@ import socket
 import threading
 import time
 
-class BroadCastServer:
+class BroadCastProxy:
     
-    def __init__(self):
-        self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.recv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.recv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    def __init__(self, port):
+        self.port = port
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-        self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.send_socket = socket.socket(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            self.socket.bind(('', 50000))
+        except socket.error, message:
+            pass
+
 
     def run(self):
-        self.read()
-        self.write()
+        t_recv = threading.Thread(target=self.read)
+        t_recv.start()
+
+        t_send = threading.Thread(target=self.write)
+        t_send.start()
 
     def read(self):
+        while True:
+            data, addr = self.socket.recvfrom(self.port)
+            print(str(data))
 
-    def 
-    
-while True:
-    time.sleep(1000)
-    s.sendto(b'I am alive!', ('255.255.255.255', 50000))
+    def write(self):
+        while True:
+            time.sleep(2)
+            self.socket.sendto(b'I am alive!\n', ('<broadcast>', self.port))
+
+proxy = BroadCastProxy(50000)
+proxy.run()
