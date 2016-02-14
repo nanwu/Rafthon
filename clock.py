e55a45a2a8fc50677d35bd64c744907f29b68f46
nanwu <nanbytesflow@gmail.com>
2016-02-13 23:36:20 -0500

    lock the clock

--- clock.py
+++ clock.py
@@ -1,24 +1,18 @@
-
 from threading import Lock
 
-class LClock:
-    """ Lamport's logical clock
-    
-    """
 
-    def init(self):
+class Clock:
+
+    def __init__(self):
         self.term = 0
         self.mutex = Lock()
 
     def incre(self):
         with self.mutex:
-            self.term += 1
+            self.term += 1 
             return self.term
 
-    def adjust(self, other_term):
+    def forward(self, other):
         with self.mutex:
-            if other_term > self.term:
-                self.term += 1
+            self.term = other
 
-
-        
--- peer.py
+++ peer.py
@@ -1,22 +1,50 @@
+import random
+import time
 from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
 
-from rafthon.status import FOLLOWER, CANDIDATE, LEADER
-from rafthon.lock import Lock
+from rafthon import status
+from rafthon.clock import Clock
 from rafthon.rpc import RPCServer
 import threading
+
+TIMEOUT_MAX = 300
+TIMEOUT_MIN = 150
+timestamp_now = lambda: int(round(time.time() * 1000))
 
 class Peer:
     
     def __init__(self, addr, port):
         self.leader = None
         self.rpc_server = RPCServer(addr, port, peer=self)
+        self.status = status.FOLLOWER
+        self.term = Clock()
 
     def append_entry(self, entry):
         pass
 
-    def request_vote(self):
-        pass
+    def request_vote(self, (addr, port), term):
+        if self.status == status.LEADER or term < self.term:
+            return False
+
+        if term == self.term and self.status == status.CANDIDATE:
+            return False
+
+        if term > self.term:
+            
+
+    def run(self):
+        self.last_event_timestamp = timestamp_now()
+        self.election_timout = random.randint(TIMEOUT_MIN, TIMEOUT_MAX)
+        while :
+            if timestamp_now() - self.last_event_timestamp >= self.election_timout:
+                self.clock.incre()
+                self.status = status.CANDIDATE
+                # issue request_vote in parralle
+                for neighbor in self.neighbors:
+
+        
+
 
 
 def main():
